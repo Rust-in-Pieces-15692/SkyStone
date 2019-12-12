30e4d394773e302e4df1de116dfbb28177eeed17
Joshua Herron <herronjo@joshiepoo.gq>
2019-12-12 15:50:04 -0500

    Okay, just pushing stuffs.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto_RedDoubleSkyStone.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto_RedDoubleSkyStone.java
@@ -40,13 +40,13 @@ public class Auto_RedDoubleSkyStone extends LinearOpMode{
                     drive(true,0.7,20);
                     mode = 0;
                 case 0:
-                    if (colorSensor.red() > colorSensor.blue() && colorSensor.green() > colorSensor.blue()) {
+                    if ((colorSensor.red() > colorSensor.blue() && colorSensor.green() > colorSensor.blue()) || (colorSensor.red() < 20 && colorSensor.green() < 20 && colorSensor.blue() < 20)) {
                         //Dude, that's a SkyStone!
                         //go get it
                         mode = 1;
                     } else {
                         //check the next one
-                        strafe(false,0.7,1);
+                        strafe(false,0.7,((float)0.5));
                     }
                     break;
                 case 1:
@@ -69,55 +69,61 @@ public class Auto_RedDoubleSkyStone extends LinearOpMode{
                     break;
                 case 2:
                     drive(false, 0.7, 28);
-                    turn(true,0.7,90);
-                    drive(true,0.7,44+movedLeft);
+                    //turn(true,0.7,90);
+                    strafe(false,0.7,44+movedLeft);
                     //drop it
-                    drive(false,0.7,(44+(movedLeft*2))); //Go back and get the other!
-                    turn(false,0.7,90);
+                    strafe(true,0.7,(44+(movedLeft*2))); //Go back and get the other!
+                    //turn(false,0.7,90);
                     drive(true,0.7,28);
                     //grab it
                     drive(false, 0.7, 28);
-                    turn(true,0.7,90);
-                    drive(true,0.7,44+movedLeft);
+                    //turn(true,0.7,90);
+                    strafe(false,0.7,44+movedLeft);
                     //drop it
                     mode = 3;
                     break;
                 case 3:
                     //go back to center (6' from bottom)
-                    drive(false,0.7,(float)33.5);
+                    strafe(true,0.7,(float)33.5);
                     //we're parked now.
             }
         }
     }
-    private void strafe(boolean right, double speed, float distance){
+    private void strafe(boolean left, double speed, float distance){
         //12.566/1440 * in to give desired ticks
-        useEncoders();
-        int tickValue = (int) Math.floor(strafeConstant*distance);
-        middleDrive.setPower(speed);
-        if (right){
+        int tickValue = (int)(strafeConstant*distance);
+        if (left){
             middleDrive.setTargetPosition(tickValue);
         } else {
             middleDrive.setTargetPosition(-tickValue);
         }
+        middleDrive.setPower(speed);
         while (middleDrive.isBusy()) {
-            continue;
+            telemetry.addData("Tick Counter", tickValue);
+            telemetry.addData("Current Position", middleDrive.getCurrentPosition());
+            telemetry.update();
         }
         middleDrive.setPower(0);
         resetEncoders();
     }
     private void drive(boolean forward, double speed, float distance){
         //12.566/1440 * in to give desired ticks
-        useEncoders();
-        int tickValue = (int) Math.floor(tickConstant*distance);
-        if (!forward){
-          tickValue *= -1;
+        int tickValue = (int)(tickConstant*distance);
+        if (forward){
+            leftDrive.setTargetPosition(tickValue);
+            rightDrive.setTargetPosition(tickValue);
+        } else {
+            leftDrive.setTargetPosition(-tickValue);
+            rightDrive.setTargetPosition(-tickValue);
         }
         leftDrive.setPower(speed);
         rightDrive.setPower(speed);
-        leftDrive.setTargetPosition(tickValue);
-        rightDrive.setTargetPosition(tickValue);
         while (leftDrive.isBusy() && rightDrive.isBusy()){
-            continue;
+            telemetry.addData("L Tick Counter", tickValue);
+            telemetry.addData("L Current Position", leftDrive.getCurrentPosition());
+            telemetry.addData("R Tick Counter", tickValue);
+            telemetry.addData("R Current Position", rightDrive.getCurrentPosition());
+            telemetry.update();
         }
         leftDrive.setPower(0);
         rightDrive.setPower(0);
@@ -130,12 +136,6 @@ public class Auto_RedDoubleSkyStone extends LinearOpMode{
         leftDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         rightDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         middleDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        leftDrive.setPower(0);
-        rightDrive.setPower(0);
-        middleDrive.setPower(0);
-    }
-    private void useEncoders(){
-        resetEncoders();
         leftDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
         rightDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
         middleDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
