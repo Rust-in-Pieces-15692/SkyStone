34a1e1ec1384e90b3cd3706b5eaf557e7fd66d12
Dominic Hupp <dhupp2020@gmail.com>
2020-01-08 17:12:12 -0500

    Created PID controller class

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto_RedStones.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto_RedStones.java
@@ -29,6 +29,7 @@
 
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
@@ -36,11 +37,16 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.Range;
 
-@TeleOp(name="Dom's secret project", group="Crazy")
+@Autonomous(name="Dom's secret project", group="Crazy")
 @Disabled
 public class Auto_RedStones extends OpMode
 {
     // Declare OpMode members.
+    private DcMotor leftDrive = null;
+    private DcMotor rightDrive = null;
+    private DcMotor middleDrive = null;
+
+    //Declare states
     private enum State {
         STATE_INITIAL,
         STATE_GOING_HOME,
@@ -52,7 +58,7 @@ public class Auto_RedStones extends OpMode
         STATE_PARKING,
         STATE_STOP
     }
-
+        private PIDController PID = new PIDController();
     private ElapsedTime runtime = new ElapsedTime();
 
     int currentXLocation = 0;
@@ -73,6 +79,9 @@ public class Auto_RedStones extends OpMode
      */
     @Override
     public void init() {
+        leftDrive = hardwareMap.get(DcMotor.class, "left_drive");
+        rightDrive = hardwareMap.get(DcMotor.class, "right_drive");
+        middleDrive = hardwareMap.get(DcMotor.class, "middle_drive");
 
     }
 
@@ -139,7 +148,8 @@ public class Auto_RedStones extends OpMode
                 }
                 else {
                     if (currentYLocation != foundationYLocation){
-                        //TODO: add Y drive
+                        PID.setPID(currentYLocation, foundationYLocation);
+                        drive();
                     }
                     else {
                         //TODO: add X drive
@@ -172,18 +182,81 @@ public class Auto_RedStones extends OpMode
      */
     @Override
     public void stop() {
+
     }
 
     private void setState(State newState){
         currentState = newState;
     }
 
-    private void strafe(boolean moveLeft, float power){
+    private void strafe(){
         //TODO: setup Strafe
     }
 
-    private void drive(boolean driveForward, float power){
-        //TODO: setup drive
+    private void drive(){
+        float speed = PID.computePID();
+        leftDrive.setPower(speed);
+        rightDrive.setPower(speed);
+    }
+
+    private class PIDController{
+        private ElapsedTime timePID = new ElapsedTime();
+        private int currentLocation;
+        private int goalLocation;
+        private float currentError;
+        private float currentTime;
+        private float previousError;
+        private float previousTime;
+        private float iTerm = 0;
+        private float kP;
+        private float kI;
+        private float kD;
+        public PIDController(){}
+        public void setPID(int currentLocation, int goalLocation){
+            this.currentLocation = currentLocation;
+            this.goalLocation = goalLocation;
+            if (currentTime == 0 && previousTime == 0){
+                timePID.reset();
+            }
+        }
+        public void reset(){
+            this.currentLocation = 0;
+            this.goalLocation = 0;
+            this.currentError = 0;
+            this.previousError = 0;
+            this.currentTime = 0;
+            this.previousError = 0;
+            this.iTerm = 0;
+            timePID.reset();
+        }
+        public void setPIDconstants(float kP, float kI, float kD){
+            this.kP = kP;
+            this.kI = kI;
+            this.kD = kD;
+        }
+        public float computePID(){
+            this.currentTime = timePID.nanoseconds();
+            this.currentError = goalLocation - currentLocation;
+
+            float pTerm = kP * currentError;
+
+            iTerm = iTerm + (kI * (currentTime - previousTime));
+
+            float dTerm = kD * ((currentError - previousError)/(currentTime - previousTime));
+
+            float power = pTerm + iTerm + dTerm;
+
+            this.previousError = currentError;
+            this.previousTime = currentTime;
+
+            if (currentError < 0.5){
+                reset();
+            }
+            telemetry.addData("Error", currentError);
+            telemetry.addData("Power", power);
+            telemetry.update();
+            return power;
+        }
     }
 
     private void stopAll(){
