425dc617e654858cabc9658e3fda588c7c58750d
Aldeenyo <38325332+Aldeenyo@users.noreply.github.com>
2020-02-10 15:14:16 -0500

    Update and rename strafe to RedOutsidePark.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RedOutsidePark.java
@@ -0,0 +1,26 @@
+package org.firstinspires.ftc.teamcode;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+import org.firstinspires.ftc.teamcode.Robot;
+
+@Autonomous(name="Red Park Outsid ", group="Red")
+public class Auto_RedOutsidePark extends LinearOpMode{
+   private Robot robot = new Robot();
+   @Override
+   public void runOpMode() {
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+        waitForStart();
+        int state = 0;
+        switch (state) {
+            case 0:
+                robot.strafe(48);
+                state = 1;
+                break;
+            case 1:
+                break;
+        }
+   }
+}
